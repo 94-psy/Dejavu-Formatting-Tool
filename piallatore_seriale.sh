fd77e374fb2b2e68739f57ed6bc5bc6c324831e5
Alessio Bevilacqua <felicecacca@gmail.com>
2020-02-03 16:22:01 +0100

    Add files via upload

---
+++ piallatore_seriale.sh
@@ -0,0 +1,377 @@
+#!/bin/bash
+casa=$HOME
+if [[ $EUID -ne 0 ]];
+then
+    exec sudo ./formatta_dischi_collegati.sh 
+fi
+#da fare: mettere la formattazione con gutmann come quarta voce del menù e, soprattutto, usare un IF per verficare che comando dare o che serie di comandi dare per evitare due funzioni
+
+
+# Find the rows and columns will default to 80x24 is it can not be detected
+screen_size=$(stty size 2>/dev/null || echo 24 80) 
+rows=$(echo $screen_size | awk '{print $1}')
+columns=$(echo $screen_size | awk '{print $2}')
+
+# Definisco le dimensioni dell'interfaccia:
+r=$(( rows / 2 ))
+c=$(( columns / 2 ))
+# e nel caso in cui lo schermo sia piccolo
+r=$(( r < 20 ? 20 : r ))
+c=$(( c < 70 ? 70 : c ))
+
+declare -a partitions=()
+spengo=true
+version="0.0.2 1/2"
+titolo="Piallatore seriale"
+server="ftp://192.168.67.66/"
+percorso_server="$server~/dejavu.img"
+percorso_server_md5="$server~/dejavu.img.md5"
+cartella_di_lavoro="$casa/.iso_dejavu"
+percorso_locale="$cartella_di_lavoro/dejavu.img"
+percorso_locale_md5="$cartella_di_lavoro/dejavu.img.md5"
+percorso_locale_md5_scaricato="$cartella_di_lavoro/dejavu_nuovo.img.md5"
+user="server"
+passwd=""
+dischi_totali=0
+
+
+function prepara_file ()
+{
+    file="$casa/.tmp_part.txt"
+    mkdir -p $cartella_di_lavoro
+    sudo chmod 777 --recursive $cartella_di_lavoro
+    rm -rf .counter
+    rm -rf '$file'
+    touch '$file'
+    
+}
+
+function scan_disks ()
+{
+    #for i in `seq 0 32`; do echo "- - -" | sudo tee /sys/class/scsi_host/host$i/scan; ls /dev/sd* ; done
+    clear
+    sudo lsblk -io KNAME,TYPE,MOUNTPOINT  >> '$file'  
+    #df -T /path |tail -n 1 |awk '{print $1}'
+    #lsblk --nodeps -no name
+
+}
+#la riga sopra mi definisce dove salvare e leggere il file
+
+
+function get_disks ()
+{
+    indice=0
+    indicei=0
+    dischi_totali=0
+    lista_dischi=()
+    prepara_file
+    scan_disks
+
+    while read nome tipo punto_di_mount #questa parte legge il file riga per riga
+    do
+        if [ "$punto_di_mount" == "/" ] ;
+        then
+            da_evitare=$nome
+        fi
+    done < '$file' #per evitare delle subshell eseguite, mi legge il file fino alla fine
+    tipo_disco="${da_evitare::-5}"
+    case "$tipo_disco" in
+	    nvme)	da_evitare="${da_evitare::-2}" ;;
+	    *) da_evitare="${da_evitare::-1}";;	
+    esac
+    ##cancella da da_evitare il numero della partizione
+    #da_evitare="${da_evitare::-1}"
+    while read nome tipo punto_di_mount #questa parte legge il file riga per riga
+    do
+        if [ "$tipo" == "disk" -a "$da_evitare" != "$nome" ] ;
+        then
+            lista_dischi+=("$nome")
+        fi
+    done < '$file' #per evitare delle subshell eseguite, mi legge il file fino alla fine
+    for i in ${lista_dischi[@]}; do
+        (( dischi_totali += 1))
+        #echo $i
+    done
+    echo "ho trovato $dischi_totali"
+}
+
+
+function formatta ()
+{
+    touch .counter
+    for i in ${lista_dischi[@]};
+    do
+        #rm log_"$i"
+        rm log_"$i" 2>/dev/null
+        #touch log_"$i" 
+        rm -rf .tmp_"$i" 2>/dev/null
+        echo "Avvio il disco /dev/$i"
+        echo "echo 'Avvio il disco /dev/$i'" >> .tmp_"$i"
+        #echo -n "comando=$()" >> .tmp_"$i"
+        #echo "sudo badblocks -fwsv /dev/$i >> log_$i" >> .tmp_"$i"
+        #echo "dd if=/dev/zero of=/dev/$i status=progress" >> .tmp_"$i"
+        #&& echo 'no error on /dev/$i at starting time $(date)'>> log_$i || echo 'error on /dev/$i at starting time $(date)'
+        echo "sleep 9" >> .tmp_"$i"
+        echo "echo '1' >> .counter" >> .tmp_"$i"
+        echo "rm .tmp_$i" >> .tmp_"$i"
+        echo "exit 0" >> .tmp_"$i"
+        chmod +x .tmp_"$i"
+        sudo x-terminal-emulator -e "./.tmp_$i" &
+        sleep 2
+        #sudo xfce4-terminal -e "./.tmp_$i" &
+        #mate-terminal -e "./.tmp_$i" &
+        #gnome-terminal --command "./.tmp_$i" &
+    done
+    rm -rf '$file'
+    
+    while [ $indice -lt ${#lista_dischi[@]} ];do
+        indice=0
+        sleep 1 #attesa prima del ricontrollo
+        while read contatore #questa parte legge il file riga per riga
+        do
+          #echo $tipo
+            (( indice += $contatore))
+            echo $(( $indice * 100 / $dischi_totali )) | dialog --title "$titolo" --gauge "Avanzamento formattazione..." $r $c 0
+        done < ".counter" #per evitare delle subshell eseguite, mi legge il file fino alla fine
+    
+    done
+    
+    
+    for i in ${lista_dischi[@]};
+    do
+    
+    if [ -s log_"$i" ] 
+    then
+    	#echo "$_file has some data."
+        smartctl -i /dev/"$i" >> log_"$i" 
+    else
+    	#echo "$_file is empty."
+        rm log_"$i" 2>/dev/null     
+    fi
+    
+    done
+    
+    
+    rm -rf .counter 2>/dev/null
+}
+    
+    
+function formattag ()
+{
+    for i in ${lista_dischi[@]};
+    do
+        #rm log_"$i"
+        #touch log_"$i" 
+        rm -rf .tmp_"$i"
+        echo "Avvio il disco /dev/$i"
+        echo "echo 'Avvio il disco /dev/$i'" >> .tmp_"$i"
+        #echo -n "comando=$()" >> .tmp_"$i"
+        #echo "sudo nwipe --autonuke --logfile=log_$i --method=gutmann /dev/$i " >> .tmp_"$i"
+        echo "echo '1' >> .counter" >> .tmp_"$i"
+        echo "rm .tmp_$i" >> .tmp_"$i"
+        echo "exit 0" >> .tmp_"$i"
+        chmod +x .tmp_"$i"
+        sudo x-terminal-emulator -e "./.tmp_$i" &
+        #mate-terminal -e "./.tmp_$i" &
+        #gnome-terminal --command "./.tmp_$i" &
+    done
+    rm -rf '$file'
+    
+    while [ $indice -lt ${#lista_dischi[@]} ];do
+        indice=0
+        sleep 1
+        while read contatore #questa parte legge il file riga per riga
+        do
+          #echo $tipo
+            (( indice += $contatore))
+            
+        done < ".counter" #per evitare delle subshell eseguite, mi legge il file fino alla fine
+    
+    done
+
+    rm -rf .counter
+}
+
+function check_img_update ()
+
+{
+    curl --user "$user":"$passwd" -o "$percorso_locale_md5_scaricato" "{$percorso_server_md5}"
+    md5sum "$percorso_locale" | awk '{ print $1}' > "$percorso_locale_md5"
+
+    md5_server=$(sudo head -n 1 "$percorso_locale_md5_scaricato") #md5sum appena scaricato
+    md5_local=$(sudo head -n 1 "$percorso_locale_md5") #md5sum preesistente
+
+    if [ "$md5_local" = "$md5_server" ] ; then
+            echo "non aggiorno"
+            #non aggiorno l'iso
+        else
+            echo "aggiorno"
+            sudo mv "$percorso_locale_md5_scaricato" "$percorso_locale_md5"
+            sudo curl --user "$user":"$passwd" -o "$percorso_locale" "{$percorso_server}"
+    fi
+
+}
+
+
+function installa ()
+
+{
+    touch .i_counter
+    for i in ${lista_dischi[@]};
+    do
+        #rm log_"$i"
+        rm log_i_"$i" 2>/dev/null
+        #touch log_"$i" 
+        rm -rf .tmp_i_"$i" 2>/dev/null
+        echo "Avvio il disco /dev/$i"
+        echo "echo 'Avvio il disco /dev/$i'" >> .tmp_i_"$i"
+        #echo "dd if=percorso_iso of=/dev/$i status=progress" >> .tmp_i_"$i"
+        echo "sleep 8" >> .tmp_i_"$i" #####
+        echo "echo '1' >> .i_counter" >> .tmp_i_"$i"
+        echo "rm .tmp_i_$i" >> .tmp_i_"$i"
+        echo "exit 0" >> .tmp_"$i"
+        chmod +x .tmp_i_"$i"
+        sudo x-terminal-emulator -e "./.tmp_i_$i" &
+
+        #sudo xfce4-terminal -e "./.tmp_i_$i" &
+        #mate-terminal -e "./.tmp_i_$i" &
+        #gnome-terminal --command "./.tmp_i_$i" &
+    done
+    rm -rf '$file'
+
+        while [ $indicei -lt ${#lista_dischi[@]} ];do
+        indicei=0
+        sleep 1 #tempo di attesa tra i controlli
+            while read contatore #questa parte legge il file riga per riga
+            do
+                (( indicei += $contatore))
+            done < ".i_counter" #per evitare delle subshell eseguite, mi legge il file fino alla fine
+            echo $(( $indicei * 100 / $dischi_totali )) | dialog --title "$titolo" --gauge "Avanzamento installazione..." $r $c 0
+            #echo $indicei
+        done
+        clear
+
+    rm -rf .i_counter 2>/dev/null 
+
+
+}
+
+function spegnimento ()
+{
+    if (whiptail  --title "$titolo" --yesno "Spengo il pc alla fine?" $r $c ); then
+        spengo=true
+    
+    else
+        spengo=false
+    fi
+}
+
+function spegni () 
+{
+if [ "$spengo" = true ]; then
+    sudo poweroff
+fi 
+}
+
+function dipendenze ()
+
+{
+    {
+        echo "\Installo curl.."
+        sudo apt intall curl -y 2>/dev/null
+        echo 33
+        echo "\Installo nwipe.."
+        sudo apt install nwipe -y 2>/dev/null
+        echo 66
+        echo "\Installo wget.."
+        sudo apt install wget -y 2>/dev/null
+        echo 100
+
+    } |whiptail --title "$titolo" --gauge "Controllo le dipendenze" $r $c 0
+}
+
+
+##inizio prorgamma##
+
+clear
+
+
+
+case "$#" in
+	1)	R=$1 ;;
+	*)	W=$(whiptail --title "Menù $titolo $version" --menu "Elenco delle funzionalità del Piallatore:" $r $c 6 \
+			"1" "Piallo i dischi, installo il sistema e me ne esco" \
+			"2" "Piallo i dischi" \
+			"3" "Installo il sistema"  3>&1 1>&2 2>&3)
+	 
+			exitstatus=$?
+			if [ $exitstatus = 0 ]; then
+				case "$W" in 
+					1)	spegnimento
+					    dipendenze
+					    get_disks 
+					    check_img_update &
+					    formatta
+					    get_disks 
+					    installa 
+					    spegni ;;
+					2)	spegnimento
+					    get_disks
+					    formatta
+					    spegni ;;
+					3)	spegnimento
+					    #dipendenze
+					    get_disks
+					    check_img_update
+					    installa
+					    spegni ;;
+					
+					*) exit 1 ;;
+				esac
+			else
+				clear 
+			fi ;;
+
+esac
+
+
+#case "$R" in
+#	1|complete|-c|--C)	    spegnimento
+#	                        dipendenze
+#	                        get_disks
+#	                        formatta
+#	                        installa ;;
+#	
+#	2|formatta|-f|--F)	    spegnimento
+#	                        get_disks
+#	                        formatta ;;
+#	
+#	3|installa|-i|--I)	    spegnimento
+#	                        dipendenze
+#	                        get_disks
+#	                        installa ;;
+#	4|Gutmann|-g|--G)	    spegnimento
+#	                        dipendenze
+#	                        get_disks
+#	                        formattag ;;
+#  	5|completeG|-cg|--CG)	spegnimento
+#  	                        dipendenze
+#  	                        get_disks
+#	                        formattag
+#	                        installa ;;
+#					
+#	-h|--help)		whiptail --title "Helpler del $titolo" --msgbox "Il programma $titolo ha 2 funzioni base: formattare i dischi e installare il sistema operativo. Se lanciato con un parametro si può bypassare l'interfaccia grafica. Usare:
+#
+#1 | complete  | -c  | --C  per eseguire tutti i passaggi
+#2 | formatta  | -f  | --F  per eseguire solo la formattazione
+#3 | installa  | -i  | --I  per eseguire solo l'installazione
+#4 | Gutmann   | -g  | --G  per eseguire la formattazione con Gutmann
+#5 | completeG | -cg | --CG per eseguire tutti i passaggi con Gutmann
+#
+#Premere INVIO per uscire" $r $c 
+#	                    exit 2;;
+#	                    
+#esac
+
+
+exit 0
